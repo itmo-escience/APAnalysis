9bfc35beca356d4d7b91edb4ebbd497fe974041f
Anton Radice <antonradice@gmail.com>
2016-06-27 13:21:12 +0400

    Update DistanceFunctions.scala

--- AG/AG-Spark/src/main/scala/AG/DistanceFunctions.scala
+++ AG/AG-Spark/src/main/scala/AG/DistanceFunctions.scala
@@ -19,7 +19,6 @@ object DistanceFunctions {
     assert(patient1.data.length == patient2.data.length, "Data size is not equal")
     val n = patient1.data.length
     var dist = 0.0
-
     for(i <- 0 until n) {
       for(j <- 0 until n) {
         dist += (patient1.data(i) - patient2.data(i)) * inverseCovariance(0,i) * (patient1.data(i) - patient2.data(i))
